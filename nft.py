d7f074060b9a7a3c5dfc86bcfc45f60e8a89992d
Gabor Hajba <gabor.hajba@prolion.com>
2024-01-09 10:00:50 +0100

    add rate limits to blockfrost calls

--- nft.py
+++ nft.py
@@ -1,5 +1,8 @@
 import json
 import os
+import time
+import functools
+from datetime import datetime, timezone
 
 import requests
 from dotenv import load_dotenv
@@ -30,10 +33,59 @@ handle_asset_address = 'https://cardano-mainnet.blockfrost.io/api/v0/assets/f0ff
 assets_for_stake = 'https://cardano-mainnet.blockfrost.io/api/v0/accounts/{}/addresses/assets?page={}'
 
 
+class RateLimiter:
+    def __init__(self, max_per_second, max_per_day, logger):
+        self.max_per_second = max_per_second
+        self.max_per_day = max_per_day
+        self.calls_made_today = 0
+        self.last_called = 0
+        self.current_day = datetime.now(timezone.utc).date()
+        self.logger = logger
+
+    def __call__(self, f):
+        @functools.wraps(f)
+        def wrapped_f(*args, **kwargs):
+            today = datetime.now(timezone.utc).date()
+
+            # Reset daily count if a new day has started
+            if today > self.current_day:
+                self.calls_made_today = 0
+                self.current_day = today
+
+            # Check daily limit
+            if self.calls_made_today >= self.max_per_day:
+                self.logger.warn("Daily request limit reached")
+                return None
+
+            # Ensure time gap between requests
+            while time.time() - self.last_called < 1.0 / self.max_per_second:
+                time.sleep(0.01)
+
+            try:
+                result = f(*args, **kwargs)
+                self.calls_made_today += 1
+                self.last_called = time.time()
+                self.logger.debug(f'{self.calls_made_today} calls made today')
+                return result
+            except Exception as e:
+                raise e
+
+        return wrapped_f
+
+
+# Initialize the rate limiter
+rate_limiter = RateLimiter(max_per_second=10, max_per_day=50000, logger=logger)
+
+
+@rate_limiter
+def _make_request(url, headers):
+    return _make_request(url, headers=headers)
+
+
 def get_addresses(policy):
     assets = []
     for i in range(1, 3):
-        response = requests.get(assets_for_policy.format(policy, i), headers=headers)
+        response = _make_request(assets_for_policy.format(policy, i), headers=headers)
         for asset in response.json():
             if policy in asset['asset']:
                 assets.append(asset['asset'])
@@ -41,7 +93,7 @@ def get_addresses(policy):
                 logger.info(json.dumps(asset, indent=4))
     addresses = []
     for asset in assets:
-        response = requests.get(asset_address.format(asset), headers=headers)
+        response = _make_request(asset_address.format(asset), headers=headers)
         for addy_info in response.json():
             if addy_info['address'] in kreate_addresses:
                 continue
@@ -54,7 +106,7 @@ def get_addresses(policy):
 def valid_handle(handle_name):
     logger.debug('resolving handle', handle_name, handle_name[1:].encode('utf-8').hex())
     logger.debug(handle_asset_address.format(handle_name[1:].encode('utf-8').hex()))
-    response = requests.get(handle_asset_address.format(handle_name[1:].encode('utf-8').hex()), headers=headers)
+    response = _make_request(handle_asset_address.format(handle_name[1:].encode('utf-8').hex()), headers=headers)
     if response.status_code != 200 or not response.json():
         logger.debug(response.json())
         logger.debug(response.status_code)
@@ -63,7 +115,7 @@ def valid_handle(handle_name):
 
 
 def get_stake_for_address(address):
-    response = requests.get(address_info.format(address), headers=headers)
+    response = _make_request(address_info.format(address), headers=headers)
     if response.status_code != 200 or not response.json():
         return None
     stake = response.json()['stake_address']
@@ -84,7 +136,7 @@ def valid_address(address_or_handle):
 def assets_for_address(stake_address):
     assets = []
     for i in range(1, 100):
-        response = requests.get(assets_for_stake.format(stake_address, i), headers=headers)
+        response = _make_request(assets_for_stake.format(stake_address, i), headers=headers)
         if response.status_code != 200 or not response.json():
             break
         assets += response.json()
